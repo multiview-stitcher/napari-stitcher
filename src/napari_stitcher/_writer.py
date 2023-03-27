c375a2fd08fbb4a6084e6e53cf7a6e0d41fda280
Marvin Albert <marvin.albert@gmail.com>
2023-03-27 18:14:57 +0200

    Fix imagej metadata

--- src/napari_stitcher/_writer.py
+++ src/napari_stitcher/_writer.py
@@ -54,18 +54,44 @@ def write_multiple(path: str, data: List[FullLayerData]) -> List[str]:
 
     array_to_write = da.stack([d[0].squeeze() for d in data])
 
-    axes = 'YX'
-    if len(channels) > 1:
-        axes = 'C' + axes
-        chunk_shape = array_to_write.shape
-    if len(times) > 1:
-        array_to_write = da.swapaxes(array_to_write, 0, 1)
-        axes = 'T' + axes
-        chunk_shape = (1, ) + array_to_write.shape[1:]
+    # axes = 'TCYX'[-array_to_write.ndim:]
+    # # chunk_shape = tuple([1] * (array_to_write.ndim - 2)) + array_to_write.shape[-2:]
+    # chunk_shape = tuple([1] * (array_to_write.ndim - 2)) + array_to_write.shape[-2:]
+
+    # axes = 'YX'
+    # if len(channels) > 1:
+    #     axes = 'C' + axes
+    #     chunk_shape = array_to_write.shape
+    #     chunk_shape = (1, ) + array_to_write.shape[1:]
+    # if len(times) > 1:
+    #     array_to_write = da.swapaxes(array_to_write, 0, 1)
+    #     axes = 'T' + axes
+    #     chunk_shape = (1, ) + array_to_write.shape[1:]
+
+    # currently hard coded and only 2d supported
+    if array_to_write[0].ndim > 2:
+        if len(data) > 1:
+            axes = 'TCYX'
+            array_to_write = da.swapaxes(array_to_write, 0, 1)
+            chunk_shape = (1, 1) + array_to_write.shape[-2:]
+        else:
+            axes = 'TYX'
+            array_to_write = array_to_write[0]
+            chunk_shape = (1, ) + array_to_write.shape[-2:]
+    else:
+        if len(data) > 1:
+            axes = 'CYX'
+            chunk_shape = (1, ) + array_to_write.shape[-2:]
+        else:
+            axes = 'YX'
+            array_to_write = array_to_write[0]
+            chunk_shape = array_to_write.shape[-2:]
 
     # input dask array contains axes order 'ctyx'
     # output tiff file should have axes order 'tcyx'
     array_to_write = array_to_write.rechunk(chunk_shape)
+
+    # import pdb; pdb.set_trace()
 
     imwrite(
         path,
@@ -97,12 +123,46 @@ if __name__ == "__main__":
     import napari
     viewer = napari.Viewer()
 
-    viewer.add_image(np.random.random((10, 10, 10)).astype(np.uint8), metadata={'metadata':{'view':-1,
-                                                               'stack_props': {'spacing': [0.1, 0.1]},
-                                                               'times': range(10)}}, name='image1_ch000')
-    viewer.add_image(np.random.random((10, 10, 10)).astype(np.uint8), metadata={'metadata':{'view':-1,
-                                                               'stack_props': {'spacing': [0.1, 0.1]},
-                                                               'times': range(10)}}, name='image1_ch001')
+    # one channel and two channels, no timepoints
 
-    write_multiple('delme.tif', [(l.data, l.metadata, '') for l in viewer.layers])
+    view_dict = {}
+    for view in range(2):
+        view_dict[view] = {"physical_pixel_sizes": [0.5]*2}
+
+    viewer.add_image(np.random.random((10, 10)).astype(np.uint8), metadata={'metadata':
+                                                              {'view':-1,
+                                                               "view_dict": view_dict,
+                                                               'stack_props': {'spacing': [0.1, 0.1]},
+                                                               'times': range(1)}}, name='image1_ch000')
+    viewer.add_image(np.random.random((10, 10)).astype(np.uint8), metadata={'metadata':
+                                                              {'view':-1,
+                                                               "view_dict": view_dict,
+                                                               'stack_props': {'spacing': [0.1, 0.1]},
+                                                               'times': range(1)}}, name='image1_ch001')
+
+    write_multiple('/Users/malbert/Desktop/delme_xy.tif', [(l.data, l.metadata, '') for l in viewer.layers[:1]])
+    write_multiple('/Users/malbert/Desktop/delme_cxy.tif', [(l.data, l.metadata, '') for l in viewer.layers[:]])
+
+    # one channel and two channels, several timepoints
+
+    viewer.layers.clear()
+    
+    view_dict = {}
+    for view in range(2):
+        view_dict[view] = {"physical_pixel_sizes": [0.5]*2}
+
+    viewer.add_image(np.random.random((5, 10, 10)).astype(np.uint8), metadata={'metadata':
+                                                              {'view':-1,
+                                                               "view_dict": view_dict,
+                                                               'stack_props': {'spacing': [0.1, 0.1]},
+                                                               'times': range(5)}}, name='image1_ch000')
+    viewer.add_image(np.random.random((5, 10, 10)).astype(np.uint8), metadata={'metadata':
+                                                              {'view':-1,
+                                                               "view_dict": view_dict,
+                                                               'stack_props': {'spacing': [0.1, 0.1]},
+                                                               'times': range(5)}}, name='image1_ch001')
+
+    write_multiple('/Users/malbert/Desktop/delme_txy.tif', [(l.data, l.metadata, '') for l in viewer.layers[:1]])
+    write_multiple('/Users/malbert/Desktop/delme_tcxy.tif', [(l.data, l.metadata, '') for l in viewer.layers[:]])
+
     # viewer.layers.save('delme.tif', plugin='napari_stitcher') # doesn't work
