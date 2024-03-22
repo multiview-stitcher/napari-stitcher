1c679c91a856b03bf22e618f5c55c194515d4e27
amichaut <arthur.michaut@pasteur.fr>
2024-03-22 20:36:14 +0100

    arrange mosaic directly from layers (not supporting multichannel images for now)

--- src/napari_stitcher/_loader_widget.py
+++ src/napari_stitcher/_loader_widget.py
@@ -43,35 +43,16 @@ class LoaderQWidget(QWidget):
 
         self.setLayout(QVBoxLayout())
 
-        self.button_load_layers_all = widgets.Button(text='All')
-        self.button_load_layers_sel = widgets.Button(text='Selected')
-        self.buttons_load_layers = widgets.HBox(
-            widgets=\
-                [self.button_load_layers_sel,
-                    self.button_load_layers_all]
-                    )
-        self.layers_selection = widgets.Select(choices=[])
-        self.load_layers_box = widgets.VBox(widgets=\
-                                            [
-            self.buttons_load_layers,
-            self.layers_selection,
-                                            ],
-                                            label='Loaded\nlayers:')
-
         # mosaic arrangement widgets
         self.overlap = widgets.FloatSlider(value=0.1, min=0, max=0.9999, label='overlap:')
         self.n_col = widgets.SpinBox(value=1, min=1, max=100, label='number of columns:') 
         self.n_row = widgets.SpinBox(value=1, min=1, max=100, label='number of rows:') 
         self.mosaic_arr = widgets.ComboBox(choices=['rows first','columns first','snake by rows','snake by columns'],
-                                             value='snake by rows', 
+                                             value='snake by columns', 
                                              label='Mosaic arrangement:')
         self.button_arrange_tiles = widgets.Button(text='Arrange tiles')
 
-        # organize widgets
-        self.loading_widgets = [
-                            self.load_layers_box,
-                            ]
-        
+        # organize widgets        
         self.mosaic_widgets = [
                             self.overlap,
                             self.n_col, 
@@ -82,7 +63,6 @@ class LoaderQWidget(QWidget):
 
 
         self.container = widgets.VBox(widgets=\
-                            self.loading_widgets+
                             self.mosaic_widgets
                             )
 
@@ -90,17 +70,7 @@ class LoaderQWidget(QWidget):
 
         self.layout().addWidget(self.container.native)
 
-        # initialize registration parameter dict
-        self.input_layers= []
-        self.msims = {}
-
-
-        # create temporary directory for storing dask arrays
-        self.tmpdir = tempfile.TemporaryDirectory()
-
-        self.button_load_layers_all.clicked.connect(self.load_layers_all)
-        self.button_load_layers_sel.clicked.connect(self.load_layers_sel)
-
+        # connect callbacks
         self.button_arrange_tiles.clicked.connect(self.arrange_tiles)
 
 
@@ -108,64 +78,6 @@ class LoaderQWidget(QWidget):
             
         self.msims = {}
         self.input_layers = []
-
-
-    def load_layers_all(self):
-
-        if not len(self.viewer.layers):
-            notifications.notification_manager.receive_info(
-                'No images in the layer list.'
-            )
-            return
-
-        self.load_layers(self.viewer.layers)
-
-
-    def load_layers_sel(self):
-
-        if not len(self.viewer.layers.selection):
-            notifications.notification_manager.receive_info(
-                'Select layers from the layer list (mutliple using shift / %s'\
-                    %('control' if ('command' in sys.platform) else 'shift')
-            )
-            return
-
-        self.load_layers([l for l in self.viewer.layers.selection])
-
-
-    def load_layers(self, layers):
-
-        self.reset()
-        self.layers_selection.choices = sorted([l.name for l in layers])
-
-        self.input_layers = [l for l in layers]
-
-        # load in layers as sims
-        for l in layers:
-
-            msim = viewer_utils.image_layer_to_msim(l, self.viewer)
-            
-            if 'c' in msim['scale0/image'].dims:
-                notifications.notification_manager.receive_info(
-                    "Layer '%s' has more than one channel.Consider splitting the stack (right click on layer -> 'Split Stack')." %l.name
-                )
-                self.layers_selection.choices = []
-                self.reset()
-                return
-            
-            msim = msi_utils.ensure_dim(msim, 't')
-            self.msims[l.name] = msim
-
-        sims = [msi_utils.get_sim_from_msim(msim) for l.name, msim in self.msims.items()]
-
-        number_of_channels = len(np.unique([
-            _utils.get_str_unique_to_ch_from_sim_coords(sim.coords)
-                for sim in sims]))
-        
-        if len(layers) and number_of_channels > 1:
-            self.link_channel_layers(layers)
-
-        #self.load_metadata()  ## remove? 
 
 
     def link_channel_layers(self, layers):
@@ -184,22 +96,18 @@ class LoaderQWidget(QWidget):
                 link_layers(ch_layers, ('contrast_limits', 'visible'))
 
     
-    def arrange_tiles(self):
+    def get_tiles_pos(self):
         """
-        Arrange tiles depending on the selected mosaic arrangement.
+        Calculate tiles' positions depending on the selected mosaic arrangement.
         """
-
-        if not len(self.msims):
-            notifications.notification_manager.receive_info(
-                'No layers loaded.'
-            )
-            return
-
-        n_tiles = len(self.msims)
 
         # get mosaic parameters assuming all tiles have the same size
-        tile_w = self.msims[self.input_layers[0].name]['scale0/image'].x.shape[0]
-        tile_h = self.msims[self.input_layers[0].name]['scale0/image'].y.shape[0]
+        l0 = self.viewer.layers[0]
+        #dims = viewer_utils.get_layer_dims(l0, self.viewer)
+        #sdims = [dim for dim in dims if dim in ['x', 'y', 'z']]  # spatial dimensions
+        # for now get tile size assuming that y and x are the last dimensions
+        tile_w = l0.extent.world[1,-1]-l0.extent.world[0,-1]  # tile width
+        tile_h = l0.extent.world[1,-2]-l0.extent.world[0,-2]  # tile height
         overlap = self.overlap.value
         n_col = self.n_col.value
         n_row = self.n_row.value
@@ -218,19 +126,37 @@ class LoaderQWidget(QWidget):
         y_array = np.linspace(tile_h/2, y_max, n_row)
 
         # get tile arrangement
-        ind_list = _utils.get_tile_indices(mosaic_arr=mosaic_arr,n_col=n_col,n_row=n_row,n_tiles=n_tiles)
+        ind_list = _utils.get_tile_indices(mosaic_arr=mosaic_arr,n_col=n_col,n_row=n_row,n_tiles=self.n_tiles)
         pos_list = []  # list of tiles' positions
         for i in range(len(ind_list)): 
             pos_list.append((x_array[ind_list[i][0]],y_array[ind_list[i][1]]))
 
-        print(pos_list)
+        return pos_list
+    
+    def arrange_tiles(self):
+        """
+        Arrange tiles in the viewer according to the selected mosaic arrangement.
+        """
+        
+        self.n_tiles = len(self.viewer.layers)  # not handling case of mulitple channels for now
+
+        if self.n_tiles == 0:
+            notifications.notification_manager.receive_info(
+                'No layers loaded.'
+            )
+            return
+        
+        # get tiles' positions
+        pos_list = self.get_tiles_pos()
+
+        for i,pos in enumerate(pos_list):
+            self.viewer.layers[i].translate[-2:] += [pos[1],pos[0]]
+
+        
 
     def __del__(self):
 
         print('Deleting napari-stitcher widget')
-
-        # clean up callbacks
-        self.viewer.dims.events.disconnect(self.update_viewer_transformations)
 
 
 if __name__ == "__main__":
